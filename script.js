a8eb5334a606c54c4624c5b77f7e86432ea9e340
bradleyham <bradleyjamesham@gmail.com>
2020-03-05 15:26:29 +1300

    update

--- script.js
+++ script.js
@@ -25,13 +25,12 @@ function displayCard(mealsObj) {
     let thumb = mealsObj.strMealThumb;
     let mealTitle = mealsObj.strMeal;
     let area = mealsObj.strArea;
-    console.log(area)
 
     section1.innerHTML =
         `  
 
     <div id="card">
-        <div class="card__title-container" ">
+        <div class="card__title-container">
           <h1 class="card__title" id='card-title'>${mealTitle}</h1>
         </div>
 
@@ -51,37 +50,45 @@ const displayInstructions = mealsObj => {
     let instructionP = document.createElement("p");
     instructionP.innerText = mealsObj.strInstructions;
 
-    section3.appendChild(instructions);
-    section3.appendChild(instructionP);
+    let div = document.createElement('div');
+    div.className = 'section-3-container'
+
+    div.appendChild(instructions);
+    div.appendChild(instructionP);
+
+    section3.appendChild(div)
 
 }
 
 
 const displayList = mealsObj => {
-    ul.innerText = '';
-    let ingredientsTitle = document.createElement('h1');
-    ingredientsTitle.innerText = "Ingredients";
-    ul.appendChild(ingredientsTitle)
-    let ingredients =
-        Object.keys(mealsObj)
-            .filter(i => i.startsWith("strIngredient"))
-            .map(i => mealsObj[i])
-            .filter(i => i !== '');
-
-    let list = ingredients.forEach(i => {
-        let li = document.createElement('li');
-
-        li.innerText = i;
+    ul.innerHTML = '';
+    let title = document.createElement('h1');
+    title.innerText = "Ingredients";
+    ul.appendChild(title)
 
 
-        ul.appendChild(li)
+    let ingredientArray = [];
+    let measureArray = [];
+
+    let ingredientKeys = Object.keys(mealsObj).filter(i => i.startsWith('strIngredient'));
+    ingredientKeys.forEach(i => { if (mealsObj[i] != "" && mealsObj[i] != " " && mealsObj[i] != null) { ingredientArray.push(mealsObj[i]) } });
+
+
+    let measureKeys = Object.keys(mealsObj).filter(i => i.startsWith('strMeasure'));
+    measureKeys.forEach(i => { if (mealsObj[i] != "" && mealsObj[i] != " " && mealsObj[i] != null) { measureArray.push(mealsObj[i]) } });
+
+    ingredientArray.forEach((i, index) => {
+        let a = document.createElement('li');
+        a.innerHTML = `${i.trim()} <span> ${measureArray[index].trim()}</span>`;
+        ul.appendChild(a)
     })
+
+
+
 }
 
-button.addEventListener("click", () => {
-    getRandomMeal()
 
-})
 
 document.addEventListener("keyup", (e) => {
     if (e.which === 13 || e.which === 32) {
@@ -89,3 +96,24 @@ document.addEventListener("keyup", (e) => {
         getRandomMeal()
     }
 })
+
+
+ // ul.innerText = '';
+    // let ingredientsTitle = document.createElement('h1');
+    // ingredientsTitle.innerText = "Ingredients";
+    // ul.appendChild(ingredientsTitle)
+
+    // let ingredients =
+    //     Object.keys(mealsObj)
+    //         .filter(i => i.startsWith("strIngredient"))
+    //         .map(i => mealsObj[i])
+    //         .filter(i => i !== '');
+
+    // let list = ingredients.forEach(i => {
+    //     let li = document.createElement('li');
+
+    //     li.innerText = i;
+
+
+    //     ul.appendChild(li)
+    // })
